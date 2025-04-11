58bbe8b45bd5ff022bda73c0518475fb8ff194b3
T-1000 <b_mar@tuta.io>
2025-04-11 15:43:16 +0200

    migrated repo

--- python/CS50_python/section_2/8_mainCalc.py
+++
@@ -1,9 +0,0 @@
-def main():
-    x = int(input("What's x? "))
-    print("x squared is", square(x))
-
-# power of 2
-def square(n):
-    return pow(n, 2)
-
-main()
--- python/CS50_python/section_5/26_alsomylib.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from pythonProjects.section_5.mylib25 import hello
-
-if len(sys.argv) == 2:
-    hello(sys.argv[1])
